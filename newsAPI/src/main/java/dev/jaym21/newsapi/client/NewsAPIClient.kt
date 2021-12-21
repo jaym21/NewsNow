cca051f3896b698a7da2874a096744f7bd1bf7fe
jaym21 <jaynmistry21@gmail.com>
2021-12-21 23:50:19 +0530

    added retrofit instance and instance of news api interface for making network calls

--- newsAPI/src/main/java/dev/jaym21/newsapi/client/NewsAPIClient.kt
+++ newsAPI/src/main/java/dev/jaym21/newsapi/client/NewsAPIClient.kt
@@ -1,4 +1,21 @@
 package dev.jaym21.newsapi.client
 
+import dev.jaym21.newsapi.models.service.NewsAPI
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
 object NewsAPIClient {
+
+    private val BASE_URL = "https://newsapi.org/v2/"
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+
+    val api by lazy {
+        retrofit.create(NewsAPI::class.java)
+    }
 }
---
+++ newsAPI/src/main/java/dev/jaym21/newsapi/models/service/NewsAPI.kt
@@ -0,0 +1,4 @@
+package dev.jaym21.newsapi.models.service
+
+interface NewsAPI {
+}
