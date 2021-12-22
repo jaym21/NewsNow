4672e9aa5ca5f8734fee6506992d965e72aa7a1e
jaym21 <jaynmistry21@gmail.com>
2021-12-22 17:37:40 +0530

    implemented updating the ui according to the data from livedata

--- app/src/main/java/dev/jaym21/newsnow/ui/MainActivity.kt
+++ app/src/main/java/dev/jaym21/newsnow/ui/MainActivity.kt
@@ -2,9 +2,11 @@ package dev.jaym21.newsnow.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import dev.jaym21.newsnow.R
 import dev.jaym21.newsnow.adapter.NewsRVAdapter
 import dev.jaym21.newsnow.databinding.ActivityMainBinding
@@ -31,10 +33,20 @@ class MainActivity : AppCompatActivity() {
         viewModel.news.observe(this, Observer { response ->
             when(response) {
                 is Resource.Success -> {
-
+                    binding?.progressBar?.visibility = View.GONE
+                    newsAdapter.submitList(response.data)
+                }
+                is Resource.Error -> {
+                    binding?.progressBar?.visibility = View.GONE
+                    if (response.data?.isNullOrEmpty() == true) {
+                        binding?.tvNoArticles?.text = response.error?.localizedMessage
+                    }
+                }
+                is Resource.Loading -> {
+                    if (response.data?.isNullOrEmpty() == true)
+                        binding?.progressBar?.visibility = View.VISIBLE
                 }
             }
-
         })
     }
 
