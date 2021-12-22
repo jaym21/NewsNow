51bdea49bb41212f9e0d20a2366f066ce2cadd13
jaym21 <jaynmistry21@gmail.com>
2021-12-22 16:12:48 +0530

    annotated AppModule with module and SingletonComponent to make lifecycle of module same as app lifecycle

--- app/src/main/java/dev/jaym21/newsnow/di/AppModule.kt
+++ app/src/main/java/dev/jaym21/newsnow/di/AppModule.kt
@@ -1,6 +1,9 @@
 package dev.jaym21.newsnow.di
 
+import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import dev.jaym21.newsnow.data.remote.service.NewsAPI
 import dev.jaym21.newsnow.utils.Constants
 import retrofit2.Retrofit
@@ -8,6 +11,8 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 //dependencies required for lifecycle of entire application
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
 
     @Provides
