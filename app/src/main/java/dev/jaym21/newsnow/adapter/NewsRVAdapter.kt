a60d8c18f64358dc2a91a570d1c07ce68087ddd5
jaym21 <jaynmistry21@gmail.com>
2021-12-24 11:50:13 +0530

    updated article recycler view layout and adapter

--- app/src/main/java/dev/jaym21/newsnow/adapter/NewsRVAdapter.kt
+++ app/src/main/java/dev/jaym21/newsnow/adapter/NewsRVAdapter.kt
@@ -9,6 +9,8 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import dev.jaym21.newsnow.R
 import dev.jaym21.newsnow.data.remote.models.entities.Article
 
@@ -22,12 +24,12 @@ class NewsRVAdapter: ListAdapter<Article, NewsRVAdapter.NewsViewHolder>(NewsDiff
         override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
             return oldItem == newItem
         }
-
     }
 
     inner class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val titleView: TextView = itemView.findViewById(R.id.tvTitle)
-        val newsImage: ImageView = itemView.findViewById(R.id.newsImage)
+        val title: TextView = itemView.findViewById(R.id.tvTitle)
+        val description: TextView = itemView.findViewById(R.id.tvDes)
+        val image: ImageView = itemView.findViewById(R.id.newsImage)
         val name: TextView = itemView.findViewById(R.id.tvName)
     }
 
@@ -37,10 +39,11 @@ class NewsRVAdapter: ListAdapter<Article, NewsRVAdapter.NewsViewHolder>(NewsDiff
 
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
         val currentItem = getItem(position)
-        holder.titleView.text = currentItem.title
+        holder.title.text = currentItem.title
+        holder.description.text = currentItem.description
         if (currentItem.source?.name != null)
             holder.name.text = currentItem.source.name
 
-        Glide.with(holder.itemView.context).load(currentItem.urlToImage).into(holder.newsImage)
+        Glide.with(holder.itemView.context).load(currentItem.urlToImage).transform(CenterCrop(), RoundedCorners(30)).into(holder.image)
     }
 }
