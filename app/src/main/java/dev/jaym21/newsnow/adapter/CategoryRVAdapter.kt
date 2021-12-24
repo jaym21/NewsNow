5f4dde0c2b3340b00791dc68cb68330ccc5c6cf6
jaym21 <jaynmistry21@gmail.com>
2021-12-24 10:30:04 +0530

    implemented showing categories recycler view in main activity

--- app/src/main/java/dev/jaym21/newsnow/adapter/CategoryRVAdapter.kt
+++ app/src/main/java/dev/jaym21/newsnow/adapter/CategoryRVAdapter.kt
@@ -1,13 +1,17 @@
 package dev.jaym21.newsnow.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import dev.jaym21.newsnow.R
 
-class CategoryRVAdapter(private val categories: MutableList<String>): RecyclerView.Adapter<CategoryRVAdapter.CategoryViewHolder>() {
+class CategoryRVAdapter(private val categories: List<String>, private val listener: ICategoryRVAdapter): RecyclerView.Adapter<CategoryRVAdapter.CategoryViewHolder>() {
+
+    private var selectedPosition = -1
 
     inner class CategoryViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val categoryName: TextView = itemView.findViewById(R.id.tvCategoryItem)
@@ -17,13 +21,36 @@ class CategoryRVAdapter(private val categories: MutableList<String>): RecyclerVi
         return CategoryViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.rv_category_item_layout, parent, false))
     }
 
-    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: CategoryViewHolder, @SuppressLint("RecyclerView") position: Int) {
 
         holder.categoryName.text = categories[position]
 
+        holder.categoryName.setOnClickListener {
+
+            listener.onCategoryClicked(categories[position])
+
+            if (selectedPosition == position) {
+                notifyItemChanged(position)
+            }
+
+            selectedPosition = position
+            notifyDataSetChanged()
+        }
+
+        if (selectedPosition == position) {
+            holder.categoryName.setTextColor(ContextCompat.getColor(holder.itemView.context, R.color.white))
+            holder.categoryName.background = ContextCompat.getDrawable(holder.itemView.context, R.drawable.category_item_selected_bg)
+        } else {
+            holder.categoryName.setTextColor(ContextCompat.getColor(holder.itemView.context, R.color.black_alpha_70))
+            holder.categoryName.background = ContextCompat.getDrawable(holder.itemView.context, R.drawable.category_item_unselected_bg)
+        }
     }
 
     override fun getItemCount(): Int {
         return categories.size
     }
+}
+
+interface ICategoryRVAdapter{
+    fun onCategoryClicked(category: String)
 }
--- app/src/main/java/dev/jaym21/newsnow/ui/MainActivity.kt
+++ app/src/main/java/dev/jaym21/newsnow/ui/MainActivity.kt
@@ -6,54 +6,69 @@ import android.view.View
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.snackbar.Snackbar
-import dev.jaym21.newsnow.R
+import dagger.hilt.android.AndroidEntryPoint
+import dev.jaym21.newsnow.adapter.CategoryRVAdapter
+import dev.jaym21.newsnow.adapter.ICategoryRVAdapter
 import dev.jaym21.newsnow.adapter.NewsRVAdapter
 import dev.jaym21.newsnow.databinding.ActivityMainBinding
 import dev.jaym21.newsnow.utils.Resource
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(), ICategoryRVAdapter {
 
     private var binding: ActivityMainBinding? = null
     private val newsAdapter = NewsRVAdapter()
     private val viewModel: NewsViewModel by viewModels()
+    private val categories = listOf("General", "Business", "Entertainment", "Sports", "Health", "Science", "Technology")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
-        setUpRecyclerView()
+        setUpCategoriesRecyclerView()
 
-        //getting news
+        setUpArticlesRecyclerView()
+
         viewModel.getNews("business")
-
 
         //observing news response using live data in news viewModel
         viewModel.news.observe(this, Observer { response ->
             when(response) {
                 is Resource.Success -> {
+                    binding?.tvNoArticles?.visibility = View.GONE
                     binding?.progressBar?.visibility = View.GONE
-                    newsAdapter.submitList(response.data)
+                    newsAdapter.submitList(response.data?.articles)
                 }
                 is Resource.Error -> {
                     binding?.progressBar?.visibility = View.GONE
-                    if (response.data?.isNullOrEmpty() == true) {
-                        binding?.tvNoArticles?.text = response.error?.localizedMessage
-                    }
+                    binding?.tvNoArticles?.visibility = View.VISIBLE
+                    binding?.tvNoArticles?.text = response.error
                 }
                 is Resource.Loading -> {
-                    if (response.data?.isNullOrEmpty() == true)
-                        binding?.progressBar?.visibility = View.VISIBLE
+                    binding?.tvNoArticles?.visibility = View.GONE
+                    binding?.progressBar?.visibility = View.VISIBLE
                 }
             }
         })
     }
 
-    private fun setUpRecyclerView() {
+    private fun setUpCategoriesRecyclerView() {
+        val categoryAdapter = CategoryRVAdapter(categories, this)
+        binding?.rvCategory?.apply {
+            adapter = categoryAdapter
+            layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.HORIZONTAL, false)
+        }
+    }
+
+    private fun setUpArticlesRecyclerView() {
         binding?.rvNews?.apply {
             adapter = newsAdapter
             layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
         }
+    }
+
+    override fun onCategoryClicked(category: String) {
+
     }
 }
