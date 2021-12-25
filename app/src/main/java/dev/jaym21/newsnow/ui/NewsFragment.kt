248946864d514926c6098ac1f92eaca904efa268
jaym21 <jaynmistry21@gmail.com>
2021-12-25 17:35:46 +0530

    added news fragment

---
+++ app/src/main/java/dev/jaym21/newsnow/ui/NewsFragment.kt
@@ -0,0 +1,20 @@
+package dev.jaym21.newsnow.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.jaym21.newsnow.R
+
+
+class NewsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_news, container, false)
+    }
+}
