854ff9454f87bec98dabcfc27940199df3c4b575
jaym21 <jaynmistry21@gmail.com>
2021-12-25 13:38:42 +0530

    added function to save and get total from datastore

--- app/src/main/java/dev/jaym21/newsnow/utils/Constants.kt
+++ app/src/main/java/dev/jaym21/newsnow/utils/Constants.kt
@@ -8,6 +8,5 @@ class Constants {
         const val API_KEY = "06327b2038d043118f222d4abf97fe9b"
         const val PAGE_SIZE_QUERY = 20
         const val DATASTORE = "datastore"
-        const val TOTAL_ARTICLES = "total_articles"
     }
 }
--- app/src/main/java/dev/jaym21/newsnow/utils/DataStoreManager.kt
+++ app/src/main/java/dev/jaym21/newsnow/utils/DataStoreManager.kt
@@ -3,9 +3,27 @@ package dev.jaym21.newsnow.utils
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.first
 
 class DataStoreManager(val context: Context) {
 
+    companion object {
+        val TOTAL_ARTICLES = intPreferencesKey("TOTAL_ARTICLES")
+    }
+
     private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = Constants.DATASTORE)
+
+    suspend fun saveTotalArticles(total: Int) {
+        context.dataStore.edit {
+            it[TOTAL_ARTICLES] = total
+        }
+    }
+
+    suspend fun getTotalArticles(): Int? {
+        val preferences = context.dataStore.data.first()
+        return preferences[TOTAL_ARTICLES]
+    }
 }
