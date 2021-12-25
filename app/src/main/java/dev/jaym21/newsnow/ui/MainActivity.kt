96e2684ee655744b8a3015b27dd1c9bab9515c55
jaym21 <jaynmistry21@gmail.com>
2021-12-25 23:45:09 +0530

    adding share button in article open layout

--- app/src/main/java/dev/jaym21/newsnow/ui/MainActivity.kt
+++ app/src/main/java/dev/jaym21/newsnow/ui/MainActivity.kt
@@ -2,30 +2,8 @@ package dev.jaym21.newsnow.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.AbsListView
-import androidx.activity.viewModels
-import androidx.lifecycle.Observer
-import androidx.lifecycle.asLiveData
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.Navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
-import dev.jaym21.newsnow.R
-import dev.jaym21.newsnow.adapter.CategoryRVAdapter
-import dev.jaym21.newsnow.adapter.ICategoryRVAdapter
-import dev.jaym21.newsnow.adapter.NewsRVAdapter
 import dev.jaym21.newsnow.databinding.ActivityMainBinding
-import dev.jaym21.newsnow.utils.Constants
-import dev.jaym21.newsnow.utils.DataStoreManager
-import dev.jaym21.newsnow.utils.Resource
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(){
