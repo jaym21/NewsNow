82085b846d7b77e4c3a2b85d3733a6153dde3c48
jaym21 <jaynmistry21@gmail.com>
2021-12-26 13:19:09 +0530

    added data store preference for theme

--- app/src/main/java/dev/jaym21/newsnow/utils/DataStoreManager.kt
+++ app/src/main/java/dev/jaym21/newsnow/utils/DataStoreManager.kt
@@ -3,6 +3,7 @@ package dev.jaym21.newsnow.utils
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
@@ -13,6 +14,7 @@ class DataStoreManager(val context: Context) {
 
     companion object {
         private val TOTAL_ARTICLES = intPreferencesKey("TOTAL_ARTICLES")
+        private val THEME = booleanPreferencesKey("THEME")
         private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = Constants.DATASTORE)
     }
 
@@ -22,8 +24,19 @@ class DataStoreManager(val context: Context) {
         }
     }
 
+    suspend fun saveIsThemeDark(isThemeDark: Boolean) {
+        context.dataStore.edit {
+            it[THEME] = isThemeDark
+        }
+    }
+
     val totalArticles: Flow<Int>
         get() = context.dataStore.data.map {
             it[TOTAL_ARTICLES] ?: 0
+        }
+
+    val isThemeDark: Flow<Boolean>
+        get() = context.dataStore.data.map {
+            it[THEME] ?: false
         }
 }
