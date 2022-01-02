eb23263591bf86cb42f5b8bbee92bece1be1a3cb
jaym21 <jaynmistry21@gmail.com>
2022-01-02 10:25:00 +0530

    added splash activity and drawable

---
+++ app/src/main/java/dev/jaym21/newsnow/ui/SplashActivity.kt
@@ -0,0 +1,11 @@
+package dev.jaym21.newsnow.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
